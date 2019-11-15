452b48b2f9d71cb462b4504e57efd30a9e3c4325
Jkanon <im.Jkanon@gmail.com>
2019-11-15 22:41:49 +0800

    新增机构管理页

---
+++ src/assets/js/inner/orgs.js
@@ -0,0 +1,61 @@
+define(["datatables", "common", "app"], function (datatable, common, APP) {
+    return {
+        API: {
+            queryPage: "/sys/orgs"
+        },
+        init: function () {
+            this.initTree();
+            this.initForm();
+        },
+        initTree: function() {
+            common.get(this.API.queryPage, {}, function(data){
+                common.initZtree($("#orgs-tree"), data, {
+                    data: {
+                        key: {
+                            name: "departName"
+                        }
+                    }
+                });
+            });
+        },
+        initForm: function(data) {
+            var templet = {
+                list: [
+                    {
+                        type: "hidden",
+                        name: "id",
+                        hiddenWhenNotExist: true
+                    }, {
+                        type: "input",
+                        label: "机构名称",
+                        name: "departName",
+                        placeholder: "请输入机构名称",
+                        required: true
+                    }, {
+                        type: "input",
+                        label: "上级机构",
+                        name: "parentId",
+                        placeholder: "请输入机构名称",
+                        required: true
+                    }, {
+                        type: "input",
+                        label: "机构编码",
+                        name: "orgCode",
+                        placeholder: "请输入机构编码",
+                        required: true
+                    }, {
+                        type: "radio",
+                        label: "机构类型",
+                        name: "sex",
+                        options: [
+                            {label: "公司", value: 1, default: true}
+                        ]
+                    }],
+                data: data || {}
+            };
+            $("#orgs-form").html(
+                template("tpl-formInput", templet || {})
+            );
+        }
+    };
+});
